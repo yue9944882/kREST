2f13d705dfeca7c657da19322f55f812dbd0814a
yue9944882 <291271447@qq.com>
2016-02-22 15:53:55 +0800

    Adding Travis Support

--- src/test/java/com/kimmin/rest/TestEntry.java
+++ src/test/java/com/kimmin/rest/TestEntry.java
@@ -1,7 +1,17 @@
 package com.kimmin.rest;
 
+import org.junit.Test;
+
 /**
  * Created by min.jin on 2016/2/22.
  */
 public class TestEntry {
+
+    /** Fake Test **/
+    @Test
+    public static void doTest(){
+        System.err.println("Test Begin...");
+    }
+
+
 }
--- src/test/java/com/kimmin/rest/example/MyResourceTest.java
+++ src/test/java/com/kimmin/rest/example/MyResourceTest.java
@@ -19,26 +19,5 @@ import com.sun.jersey.api.core.ResourceConfig;
 
 public class MyResourceTest {
 
-    public static URI getBaseURI(){
-        return UriBuilder.fromUri("http://localhost/").port(9998).build();
-    }
 
-    public static final URI BASE_URI = getBaseURI();
-
-    protected static HttpServer startServer() throws IllegalArgumentException, NullPointerException, IOException{
-        System.out.println("Start server...");
-        ResourceConfig config = new PackagesResourceConfig("com.study.jersey.server");
-        return GrizzlyServerFactory.createHttpServer(BASE_URI, config);
-    }
-    public static void main(String[] args) {
-        try {
-            HttpServer httpServer = startServer();
-            System.out.println(String.format("Jersey app started with WADL available at" + "%sapplication.wadl\nTry out %shelloworld\nHit enter to stop it...", BASE_URI, BASE_URI));
-            System.in.read();
-            httpServer.stop();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
-    }
 }
